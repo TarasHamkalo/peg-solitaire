125d6c5e874eca1f78c9fb5ccc6532af1f29ab36
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2024-03-01 22:42:50 +0100

    Event manager implemented.

--- src/main/java/pegsolitaire/game/core/board/events/BoardEventManager.java
+++ src/main/java/pegsolitaire/game/core/board/events/BoardEventManager.java
@@ -4,4 +4,5 @@ public interface BoardEventManager {
     void subscribe(BoardEvent.Type type, BoardEventHandler handler);
     void unsubscribe(BoardEvent.Type type, BoardEventHandler handler);
     void publish(BoardEvent event);
+    void clearAll();
 }
--- src/main/java/pegsolitaire/game/core/board/events/impl/BoardEventManagerImpl.java
+++ src/main/java/pegsolitaire/game/core/board/events/impl/BoardEventManagerImpl.java
@@ -1,22 +1,43 @@
 package pegsolitaire.game.core.board.events.impl;
 
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
 import pegsolitaire.game.core.board.events.BoardEvent;
 import pegsolitaire.game.core.board.events.BoardEventHandler;
 import pegsolitaire.game.core.board.events.BoardEventManager;
 
+import java.util.*;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class BoardEventManagerImpl implements BoardEventManager {
+    Map<BoardEvent.Type, Set<BoardEventHandler>> eventToHandlers;
+    public BoardEventManagerImpl() {
+        this.eventToHandlers = new EnumMap<>(BoardEvent.Type.class);
+    }
+
     @Override
     public void subscribe(BoardEvent.Type type, BoardEventHandler handler) {
-        throw new IllegalStateException();
+        if (this.eventToHandlers.getOrDefault(type, Collections.emptySet()).contains(handler)) {
+            return;
+        }
+
+        this.eventToHandlers.computeIfAbsent(type, t -> new HashSet<>()).add(handler);
     }
 
     @Override
     public void unsubscribe(BoardEvent.Type type, BoardEventHandler handler) {
-        throw new IllegalStateException();
+        this.eventToHandlers.computeIfAbsent(type, t -> new HashSet<>()).remove(handler);
     }
 
     @Override
     public void publish(BoardEvent event) {
-        throw new IllegalStateException();
+        this.eventToHandlers
+            .getOrDefault(event.getEventType(), Collections.emptySet())
+            .forEach(handler -> handler.handle(event.getTriggerCommand()));
+    }
+
+    @Override
+    public void clearAll() {
+        this.eventToHandlers.clear();
     }
 }
