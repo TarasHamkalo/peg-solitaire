6c17b9db9c917f35c73a6ea5ada3d4d085dd8fc1
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2024-03-01 22:46:08 +0100

    ConsoleUI implemented.

--- src/main/java/pegsolitaire/game/ui/ConsoleUI.java
+++ src/main/java/pegsolitaire/game/ui/ConsoleUI.java
@@ -1,218 +1,26 @@
 package pegsolitaire.game.ui;
 
-import com.github.kwhat.jnativehook.GlobalScreen;
-import com.github.kwhat.jnativehook.NativeHookException;
-import lombok.AccessLevel;
-import lombok.Data;
 import lombok.NonNull;
-import lombok.experimental.FieldDefaults;
-import lombok.experimental.NonFinal;
 import pegsolitaire.game.core.Game;
-import pegsolitaire.game.core.board.Board;
 import pegsolitaire.game.core.board.BoardCell;
-import pegsolitaire.game.core.board.Color;
 
-import java.util.Arrays;
+// TODO: remove stop
+public interface ConsoleUI {
+    void printBoard(@NonNull BoardCell[][] cells);
 
+    void positionPrompt();
 
-@Data
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
-public class ConsoleUI {
-    public static final String CSI_SEQ = "\033[";
-    public static final String CLEAR_SCREEN = "\033[2J";
-    public static final String POSITION_FORMAT = "%d;%dH";
-    private static final Color MAIN_CURSOR = Color.BLUE;
-    private static final Color POSSIBLE_MOVE_CURSOR = Color.GREEN;
-    private static final Color SELECTED_CURSOR = Color.BRIGHT_MAGENTA;
-    private static final int BOARD_OFFSET_Y = 1;
-    private static final int BOARD_OFFSET_X = 3;
+    void clearScreen();
 
-    @NonNull Game game;
-    @NonNull KeyboardListener keyboardListener;
-    @NonFinal
-    int x;
-    @NonFinal
-    int y;
+    void start(@NonNull Game game);
 
-    public ConsoleUI(@NonNull Game game) {
-        this.keyboardListener = new KeyboardListener(this);
-        this.game = game;
-    }
+    void stop();
 
-    private static void positionAtScreen(int x, int y) {
-        System.out.printf(CSI_SEQ + POSITION_FORMAT, y, x);
-    }
+    void exit();
 
-    public void start() throws NativeHookException {
-        GlobalScreen.registerNativeHook();
-        GlobalScreen.addNativeKeyListener(this.keyboardListener);
+    void adjustCursor(int dx, int dy);
 
-        this.game.start();
-        var boardCells = this.game.getBoard().getBoardCells();
-        this.x = (boardCells[0].length + 1) / 2;
-        this.y = 0;
-//        boolean running = true;
-//        while (running) {
-//            setup();
-//            play();
-//        }
-    }
+    void select();
 
-    public void stop() throws NativeHookException {
-        GlobalScreen.removeNativeKeyListener(this.keyboardListener);
-        GlobalScreen.unregisterNativeHook();
-    }
-
-    private void setup() {
-    }
-
-    private void play() {
-    }
-
-    public void select() {
-        if (this.game.isPegSelected()) {
-            processMove();
-        }
-
-        if (this.game.selectPeg(this.x, this.y)) {
-            this.game.getPossibleMoves().forEach(pos -> {
-                var cell = this.game.getBoard().getBoardCellAt(pos[0], pos[1]);
-                printCharsAroundCell(
-                    pos[0], pos[1], '(', ')', cell.getState().getColor(), Color.GREEN
-                );
-            });
-
-            positionLogically(this.x, this.y);
-        }
-    }
-
-    private void processMove() {
-        if (this.game.makeMove(this.x, this.y)) {
-            // TODO: implement move
-        }
-
-    }
-
-    public void adjustCursor(int x, int y) {
-        var dx = this.x + x;
-        var dy = this.y + y;
-        var nextCell = this.game.getBoard().getBoardCellAt(dx, dy);
-        if (nextCell != null) {
-            var currentCell = this.game.getBoard().getBoardCellAt(this.x, this.y);
-            printCellAt(currentCell, this.x, this.y);
-            printCharsAroundCell(
-                dx, dy, '[', ']', nextCell.getState().getColor(), MAIN_CURSOR
-            );
-            positionLogically(dx, dy);
-
-            this.x = dx;
-            this.y = dy;
-        }
-    }
-
-    private int[] logicalXYToScreen(int x, int y) {
-        return new int[]{
-            3 * (x + 1) - 2 + BOARD_OFFSET_X, y + 1 + BOARD_OFFSET_Y
-        };
-    }
-
-    private void positionLogically(int x, int y) {
-        var pos = logicalXYToScreen(x, y);
-        positionAtScreen(pos[0], pos[1]);
-    }
-
-    private void printCellAt(BoardCell cell, int x, int y) {
-        positionLogically(x, y);
-        printCell(cell);
-        if (this.game.isPegSelected()) {
-            var selectedPos = this.game.getSelectedPegPosition();
-
-            if (selectedPos[0] == x && selectedPos[1] == y) {
-                printCharsAroundCell(x, y, '{', '}', cell.getState().getColor(), SELECTED_CURSOR);
-            } else {
-                this.game.getPossibleMovesFromSelectedPosition().parallelStream()
-                    .filter(pos -> pos[0] == x && pos[1] == y)
-                    .findAny().ifPresent(pos -> {
-                        printCharsAroundCell(
-                            x, y, '(', ')', cell.getState().getColor(), POSSIBLE_MOVE_CURSOR
-                        );
-                    });
-
-            }
-        }
-    }
-
-    public void printCharsAroundCell(int x, int y, char left, char right, Color fg, Color bg) {
-        var pos = logicalXYToScreen(x, y);
-        System.out.print(bg.getCode() + fg.getCode());
-
-        positionAtScreen(pos[0], pos[1]);
-        System.out.print(left);
-        positionAtScreen(pos[0] + 2, pos[1]);
-        System.out.print(right);
-
-        resetColor();
-    }
-
-
-    private void printCell(BoardCell cell) {
-        if (cell == null) {
-            System.out.print("   ");
-            return;
-        }
-
-        var peg = cell.getPeg();
-        if (peg != null) {
-            System.out.print(
-                cell.getState().getColor() + peg.getColor().toString() + " o " + Color.RESET
-            );
-        } else {
-            System.out.print(cell.getState().getColor() + " . " + Color.RESET);
-        }
-    }
-
-    public void printBoard() {
-        if (!game.isStarted()) {
-            // LOG
-            return;
-        }
-
-        clearScreen();
-        printColumnNumberLine();
-        BoardCell[][] boardCells = game.getBoard().getBoardCells();
-        for (int i = 0; i < boardCells.length; i++) {
-            System.out.printf("%s%s", Color.BRIGHT_BLACK_BG, Color.BLUE);
-            System.out.printf((i + 1) + "~>");
-            resetColor();
-
-            for (int j = 0; j < boardCells[i].length; j++) {
-                printCell(boardCells[i][j]);
-            }
-
-            System.out.println();
-        }
-
-        resetColor();
-    }
-
-    private void printColumnNumberLine() {
-        var colsAmount = this.game.getBoard().getBoardCells()[0].length;
-        System.out.printf("%s%s   ", Color.BRIGHT_BLACK_BG, Color.BLUE);
-        for (int i = 0; i < colsAmount; i++) {
-            System.out.printf("/%d\\", i + 1);
-        }
-
-        System.out.println();
-        resetColor();
-    }
-
-
-    private void clearScreen() {
-        System.out.print(ConsoleUI.CLEAR_SCREEN);
-        System.out.print(ConsoleUI.CSI_SEQ + "H");
-    }
-
-    private void resetColor() {
-        System.out.print(Color.RESET);
-    }
+    void undo();
 }
---
+++ src/main/java/pegsolitaire/game/ui/impl/ConsoleUIImpl.java
@@ -0,0 +1,231 @@
+package pegsolitaire.game.ui.impl;
+
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.SneakyThrows;
+import lombok.experimental.FieldDefaults;
+import pegsolitaire.game.core.Game;
+import pegsolitaire.game.core.board.BoardCell;
+import pegsolitaire.game.core.board.Color;
+import pegsolitaire.game.ui.ConsoleUI;
+import pegsolitaire.game.ui.KeyboardListener;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ConsoleUIImpl implements ConsoleUI {
+    private static final Color MAIN_CURSOR = Color.BLUE;
+    private static final Color POSSIBLE_MOVE_CURSOR = Color.GREEN;
+    private static final Color SELECTED_CURSOR = Color.BRIGHT_MAGENTA;
+
+    private static final int POSSIBLE_MOVE_CODE = 1;
+    private static final int SELECTED_CURSOR_CODE = 2;
+    private static final int BOARD_OFFSET_Y = 0;
+    private static final int BOARD_OFFSET_X = 0;
+
+    PrintStream output = new PrintStream(new FileOutputStream("terminal.out"));
+    KeyboardListener keyboardListener;
+    Game game;
+    // 0 none, 1 possible move, 2 selected
+    int[][] boardSelections;
+    int x;
+    int y;
+
+    public ConsoleUIImpl() throws NativeHookException, FileNotFoundException {
+        GlobalScreen.registerNativeHook();
+    }
+
+    public void start(@NonNull Game game) {
+        this.game = game;
+        this.keyboardListener = new KeyboardListener(this);
+        GlobalScreen.addNativeKeyListener(this.keyboardListener);
+
+        this.game.start();
+
+        var cells = this.game.getBoard().getBoardCells();
+        this.boardSelections = new int[cells.length][cells[0].length];
+        output.println(boardSelections[0].length);
+        this.x = (cells[0].length) / 2;
+        this.y = cells.length / 2;
+
+        System.out.print("\033[?25l");
+        clearScreen();
+        printBoard(cells);
+        printCursor(x, y, MAIN_CURSOR);
+    }
+
+    @Override
+    public void stop() {
+        GlobalScreen.removeNativeKeyListener(this.keyboardListener);
+        this.game.stop();
+        System.out.print("\033[?25h");
+        clearScreen();
+    }
+
+
+    @Override
+    @SneakyThrows
+    public void exit() {
+        stop();
+        GlobalScreen.unregisterNativeHook();
+    }
+
+    /*
+        Cursor adjustment
+    */
+    @Override
+    public void adjustCursor(int x, int y) {
+        var dx = this.x + x;
+        var dy = this.y + y;
+        var nextCell = this.game.getBoard().getBoardCellAt(dx, dy);
+        if (nextCell != null) {
+            var currentCell = this.game.getBoard().getBoardCellAt(this.x, this.y);
+
+            saveCursor();
+            output.println(this.x + " " + this.y);
+
+            printCellAt(currentCell, this.x, this.y);
+            printCursor(dx, dy, MAIN_CURSOR);
+
+            restoreCursor();
+
+            this.x = dx;
+            this.y = dy;
+        }
+    }
+
+
+    private void printCellAt(BoardCell cell, int x, int y) {
+        int[] pos = logicalXYToScreen(this.x, this.y);
+        positionAtScreen(pos[0], pos[1]);
+        System.out.print(cell);
+        if (this.boardSelections[y][x] == POSSIBLE_MOVE_CODE) {
+            printCursor(x, y, POSSIBLE_MOVE_CURSOR);
+        } else if (this.boardSelections[y][x] == SELECTED_CURSOR_CODE) {
+            printCursor(x, y, SELECTED_CURSOR);
+        }
+    }
+
+    @Override
+    public void select() {
+        if (processMove()) {
+            return;
+        }
+
+        if (this.game.selectPeg(x, y)) {
+            this.boardSelections[y][x] = 2;
+            this.saveCursor();
+
+            this.printCursor(x, y, SELECTED_CURSOR);
+            this.game.getPossibleMoves().forEach(pos -> {
+                this.boardSelections[pos[1]][pos[0]] = 1;
+                this.printCursor(pos[0], pos[1], POSSIBLE_MOVE_CURSOR);
+            });
+
+            this.restoreCursor();
+        }
+    }
+
+    private boolean processMove() {
+        if (!this.game.isPegSelected()) {
+            return false;
+        }
+
+        var boardCells = this.game.getBoard().getBoardCells();
+        var res = this.game.makeMove(x, y);
+        output.println(res);
+        saveCursor();
+
+        this.boardSelections = new int[boardCells.length][boardCells[0].length];
+        System.out.print("\033[H");
+        printBoard(boardCells);
+
+        restoreCursor();
+        return res;
+    }
+
+    @Override
+    public void undo() {
+        if (this.game != null && this.game.isStarted()) {
+            this.game.undoMove();
+            saveCursor();
+            var boardCells = this.game.getBoard().getBoardCells();
+            this.boardSelections = new int[boardCells.length][boardCells[0].length];
+            System.out.print("\033[H");
+            printBoard(boardCells);
+            printCursor(this.x, this.y, MAIN_CURSOR);
+            restoreCursor();
+        }
+    }
+
+    /*
+        Screen positioning and printing
+    */
+    public void printBoard(@NonNull BoardCell[][] cells) {
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells[i].length; j++) {
+                System.out.print((cells[i][j] == null) ? "   " : cells[i][j]);
+            }
+
+            System.out.println(Color.RESET);
+        }
+    }
+
+    private void printCursor(int x, int y, Color color) {
+        int[] position = logicalXYToScreen(x, y);
+        char[] brackets = {'[', ']'};
+        System.out.print(color);
+        if (POSSIBLE_MOVE_CURSOR.equals(color)) {
+            brackets = new char[]{'(', ')'};
+        } else if (SELECTED_CURSOR.equals(color)) {
+            brackets = new char[]{'{', '}'};
+        }
+
+        positionAtScreen(position[0], position[1]);
+        System.out.print(brackets[0]);
+        positionAtScreen(position[0] + 2, position[1]);
+        System.out.print(brackets[1] + Color.RESET.getCode());
+
+    }
+
+    private static void positionAtScreen(int x, int y) {
+        System.out.printf("\033[%d;%dH", y, x);
+    }
+
+    private int[] logicalXYToScreen(int x, int y) {
+        return new int[]{
+            3 * (x + 1) - 2 + BOARD_OFFSET_X, y + 1 + BOARD_OFFSET_Y
+        };
+    }
+
+    @Override
+    public void positionPrompt() {
+        if (this.game != null && this.game.isStarted()) {
+            int propmtY = this.game.getBoard().getBoardCells().length + BOARD_OFFSET_Y + 2;
+            positionAtScreen(0, propmtY);
+        } else {
+            positionAtScreen(0, 1);
+        }
+    }
+
+    private void restoreCursor() {
+        System.out.print("\033[u");
+    }
+
+    private void saveCursor() {
+        System.out.print("\033[s");
+    }
+
+    public void clearScreen() {
+        System.out.print("\033[2J");
+        System.out.print("\033[0H");
+    }
+
+}
