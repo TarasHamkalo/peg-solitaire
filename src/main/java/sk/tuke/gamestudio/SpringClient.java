ed711214fce490bc25ed7a311f6253e1ea37ed7a
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2024-04-01 00:04:28 +0200

    Added RatingServiceJPA.

--- src/main/java/sk/tuke/gamestudio/SpringClient.java
+++ src/main/java/sk/tuke/gamestudio/SpringClient.java
@@ -1,31 +1,38 @@
 package sk.tuke.gamestudio;
 
+import jakarta.persistence.EntityManager;
 import lombok.SneakyThrows;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.*;
 import sk.tuke.gamestudio.pegsolitaire.game.core.pegs.impl.PegFactoryImpl;
 import sk.tuke.gamestudio.pegsolitaire.game.ui.BoardUI;
 import sk.tuke.gamestudio.pegsolitaire.game.ui.InputHandler;
 import sk.tuke.gamestudio.pegsolitaire.game.ui.Prompt;
 import sk.tuke.gamestudio.pegsolitaire.game.ui.impl.BoardUIImpl;
 import sk.tuke.gamestudio.pegsolitaire.game.ui.impl.PromptImpl;
+import sk.tuke.gamestudio.service.RatingService;
 import sk.tuke.gamestudio.service.ScoreService;
+import sk.tuke.gamestudio.service.impl.RatingServiceJPA;
 
+import javax.swing.text.html.parser.Entity;
 
 @SpringBootApplication
+@Configuration
 public class SpringClient {
     public static void main(String[] args) {
         SpringApplication.run(SpringClient.class, args);
     }
 
     @Bean
+    @Profile("prod")
     public CommandLineRunner commandLineRunner(Prompt prompt) {
-        return (strings) -> prompt.begin();
+        return strings -> prompt.begin();
     }
 
     @Bean
+    @Profile("prod")
     public Prompt prompt(BoardUI boardUI, InputHandler serviceInputHandlers) {
         return PromptImpl.builder()
             .additionalInputHandler(serviceInputHandlers)
@@ -36,14 +43,19 @@ public class SpringClient {
 
     @Bean
     @SneakyThrows
+    @Profile("prod")
     public BoardUI boardUI(ScoreService scoreService) {
         return new BoardUIImpl(scoreService);
     }
 
-
     @Bean
+    @Profile("prod")
     public InputHandler serviceInputHandlers(InputHandler commentCommandHandler, InputHandler... handlers) {
         return InputHandler.link(commentCommandHandler, handlers);
     }
 
+    @Bean
+    public RatingService ratingService(EntityManager entityManager) {
+        return new RatingServiceJPA(entityManager);
+    }
 }
---
+++ src/main/java/sk/tuke/gamestudio/service/impl/RatingServiceJPA.java
@@ -0,0 +1,94 @@
+package sk.tuke.gamestudio.service.impl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.PersistenceException;
+import jakarta.persistence.TypedQuery;
+import jakarta.transaction.Transactional;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import sk.tuke.gamestudio.entity.Rating;
+import sk.tuke.gamestudio.exception.RatingException;
+import sk.tuke.gamestudio.service.RatingService;
+
+
+@Repository
+@Transactional
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RatingServiceJPA implements RatingService {
+
+    EntityManager entityManager;
+
+    @Autowired
+    public RatingServiceJPA(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void setRating(Rating rating) throws RatingException {
+        var builder = entityManager.getCriteriaBuilder();
+        var criteria = builder.createQuery(Rating.class);
+        var from = criteria.from(Rating.class);
+
+        var findQuery = entityManager.createQuery(criteria.select(from).where(
+            builder.equal(from.get("player"), rating.getPlayer()),
+            builder.equal(from.get("game"), rating.getGame())
+        ));
+
+        try {
+            updateOrPersistRating(rating, findQuery);
+        } catch (PersistenceException e) {
+            throw new RatingException("Was not able to set rating", e);
+        }
+    }
+
+    private void updateOrPersistRating(Rating rating, TypedQuery<Rating> findQuery) {
+        try {
+            var retrieved = findQuery.getSingleResult();
+            retrieved.setStars(rating.getStars());
+            retrieved.setRatedOn(rating.getRatedOn());
+
+        } catch (NoResultException ratingDoesNoExist) {
+            entityManager.persist(rating);
+        }
+    }
+
+    @Override
+    public int getAverageRating(String game) throws RatingException {
+        double rating = entityManager.createQuery(
+            "SELECT AVG(stars) FROM Rating r WHERE r.game = :game", Double.class
+            )
+            .setParameter("game", game)
+            .getSingleResult();
+        return (int) rating;
+    }
+
+    @Override
+    public int getRating(String game, String player) throws RatingException {
+        var builder = entityManager.getCriteriaBuilder();
+        var criteria = builder.createQuery(Integer.class);
+        var from = criteria.from(Rating.class);
+
+        var selectStarsQuery = entityManager.createQuery(criteria.select(from.get("stars")).where(
+                builder.equal(from.get("player"), player),
+                builder.equal(from.get("game"), game)
+        ));
+
+        try {
+            return selectStarsQuery.getSingleResult();
+        } catch (PersistenceException e) {
+            throw new RatingException("Was not able to retrieve rating", e);
+        }
+    }
+
+    @Override
+    public void reset() throws RatingException {
+        try {
+            entityManager.createQuery("DELETE FROM Rating r").executeUpdate();
+        } catch (PersistenceException e) {
+            throw new RatingException("Was not able to delete ratings", e);
+        }
+    }
+}
