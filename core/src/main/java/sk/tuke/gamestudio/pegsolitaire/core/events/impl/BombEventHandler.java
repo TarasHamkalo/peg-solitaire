10d1a2df194d9355061954e913d50da553f2cb76
Taras Hamkalo <taras.hamkalo@student.tuke.sk>
2024-04-05 23:16:54 +0200

    Added core module: separates all core logic classes

---
+++ core/src/main/java/sk/tuke/gamestudio/pegsolitaire/core/events/impl/BombEventHandler.java
@@ -0,0 +1,28 @@
+package sk.tuke.gamestudio.pegsolitaire.core.events.impl;
+
+import sk.tuke.gamestudio.pegsolitaire.core.commands.impl.RemoveCommand;
+import sk.tuke.gamestudio.pegsolitaire.core.Direction;
+import sk.tuke.gamestudio.pegsolitaire.core.events.BoardEvent;
+import sk.tuke.gamestudio.pegsolitaire.core.events.BoardEventHandler;
+
+public class BombEventHandler implements BoardEventHandler {
+
+    @Override
+    public void handle(BoardEvent event) {
+        var command = event.getTriggerCommand();
+        if (command instanceof RemoveCommand) {
+            return;
+        }
+
+        int[] currentPosition = command.getFinalPosition();
+        for (Direction direction : Direction.values()) {
+            int[] destroyPosition = new int[]{
+                currentPosition[0] + direction.getX(), currentPosition[1] + direction.getY()
+            };
+
+            command.getBoard().destroyCell(destroyPosition);
+        }
+
+        command.getBoard().destroyCell(currentPosition);
+    }
+}
---
+++ core/src/main/java/sk/tuke/gamestudio/pegsolitaire/core/levels/impl/SmallFrogLevelBuilder.java
@@ -0,0 +1,31 @@
+package sk.tuke.gamestudio.pegsolitaire.core.levels.impl;
+
+import lombok.NonNull;
+import sk.tuke.gamestudio.pegsolitaire.core.board.BoardCell;
+import sk.tuke.gamestudio.pegsolitaire.core.board.impl.BasicCell;
+import sk.tuke.gamestudio.pegsolitaire.core.levels.LevelBuilder;
+import sk.tuke.gamestudio.pegsolitaire.core.pegs.PegFactory;
+
+// THE ONLY LEVEL I MANAGED TO BEAT
+public class SmallFrogLevelBuilder extends LevelBuilder {
+    public SmallFrogLevelBuilder(@NonNull PegFactory pegFactory) {
+        super(pegFactory);
+    }
+
+    @Override
+    public BoardCell[][] build() {
+        BoardCell[][] boardCells = new BoardCell[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                boardCells[i][j] = new BasicCell();
+            }
+        }
+
+        boardCells[0][0].setPeg(this.getPegFactory().getRandomPeg());
+        boardCells[0][1].setPeg(this.getPegFactory().getRandomPeg());
+        boardCells[1][2].setPeg(this.getPegFactory().getRandomPeg());
+        boardCells[2][1].setPeg(this.getPegFactory().getRandomPeg());
+
+        return boardCells;
+    }
+}
