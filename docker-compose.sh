e245499dfacc6197baeccbfaecbd421613086a14
Taras Hamkalo <hamkalo.taras@gmail.com>
2024-09-25 10:09:16 +0200

    fix(constants): changed ports

--- docker-compose.sh
+++ docker-compose.sh
@@ -4,5 +4,8 @@ docker run -d --rm \
   --name users-and-feedback-db \
   -e POSTGRES_USER=taras \
   -e POSTGRES_PASSWORD=taras \
+  -e POSTGRES_DB=gamestudio \
+  -v pg_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
   postgres 
 
--- game-server/src/main/resources/static/js/constants.js
+++ game-server/src/main/resources/static/js/constants.js
@@ -6,7 +6,7 @@ const config = {
     "secret": "secret",
     "response_type": "code",
     "scope": "openid profile",
-    "redirect_uri": "http://localhost:80/",
+    "redirect_uri": "http://localhost:9090/",
     "client_authorization": "Basic Y2xpZW50OnNlY3JldA=="
 };
 
@@ -17,7 +17,7 @@ authorizationUrl.searchParams.set("scope", config["scope"]);
 authorizationUrl.searchParams.set("response_type", config["response_type"]);
 authorizationUrl.searchParams.set("redirect_uri", config["redirect_uri"]);
 
-const gameServerUrl = "http://localhost/pegsolitaire";
+const gameServerUrl = "http://localhost:9090/pegsolitaire";
 const movesUrl = new URL(gameServerUrl + "/moves");
 const selectUrl = new URL(gameServerUrl + "/select?x=0&y=0");
 const moveUrl = new URL(gameServerUrl + "/move?x=0&y=0");
